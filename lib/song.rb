ab4126bde2d8acaba3b82f180e3b97f3ae42f2a1
Bria White <bria.awhite@gmail.com>
2020-08-14 18:08:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,5 +5,14 @@ class Song
   @@all = []
   
   def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist
+    @genre = genre
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
   
 end 
