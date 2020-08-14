7ab7b2df323ff2b7019a2cf6464f764f4984f5d4
Bria White <bria.awhite@gmail.com>
2020-08-14 18:20:35 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,32 @@
+class Doctor 
+  
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def new_appointment(name, patient)
+    Appointment.new(name, self, patient)
+  end 
+  
+  def appointments 
+    Appointment.all.select do |appointment|
+      appointment.patient == self 
+    end 
+  end 
+  
+  def patients 
+    appointments.map do |appointment|
+      appointment.patient 
+    end 
+  end
+  
+end 
